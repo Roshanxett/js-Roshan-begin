551d03fd11139a0d9ddedb7317c1d6256d35c76f
Roshanxett <122327396+Roshanxett@users.noreply.github.com>
2024-09-29 22:44:33 +0000

    Datatypes done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 122121
+let accountEmail = "roshanbalan22@gmail.com"
+var accountPassword = "Roshan"
+accountCity = "Dang"
+gender = "male"
+console.log(accountCity);
+console.table([accountId, accountEmail, accountPassword, accountCity, gender])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//console.log(3+3)
+let name ="roshan"
+let age = 23
+let isLoggedIn = true
+let state;
+console.log(typeof isLoggedIn)
